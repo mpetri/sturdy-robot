3380e30f21250d336b5c3017511b1548795cc488
sangclee17 <sangclee17@gmail.com>
2017-01-20 20:18:47 +1100

    Update sampleproj.go
    Implemented/modified lineWordCount method

--- sampleproj.go
+++ sampleproj.go
@@ -1,101 +1,125 @@
 package main
 
-import(
-    "flag"
-    "fmt"
-    "os"
-    "log"
-    "path/filepath"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 	"regexp"
-    "bufio"
-    "time"
+	"strings"
+	"time"
 )
 
-type txtFile struct{
-    fname string
-    numLines int
-    //wordCount int   
+type txtFile struct {
+	fname     string
+	lineCount int
+	wordCount int
 }
 
-func lineCounter(fname string) (int, error) {
+func lineWordCounter(fname, keyWord string) (int, int, error) {
 	file, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	var counter int
+
 	scanner := bufio.NewScanner(file)
+	var lineCount int
+	var lines []string
+
 	for scanner.Scan() {
-		counter++
+		line := scanner.Text()
+		lines = append(lines, line)
+		lineCount++
 	}
-	return counter, scanner.Err()
+	wordCount := findKeyWord(lines, keyWord)
+	return lineCount, wordCount, scanner.Err()
 }
-func printFile(fp *os.File) filepath.WalkFunc{
-    return func(path string, f os.FileInfo, err error) error{
-        if err != nil {
-            log.Print(err)
-            return nil
-        }
-        if !f.IsDir() {
+
+func findKeyWord(lines []string, keyWord string) int {
+	wordCount := make(map[string]int)
+	for _, line := range lines {
+		words := strings.FieldsFunc(line, Split)
+		for _, word := range words {
+			wordLow := strings.ToLower(word)
+			(wordCount[wordLow])++
+		}
+	}
+	keyWordLow := strings.ToLower(keyWord)
+	if count, ok := wordCount[keyWordLow]; ok {
+		return count
+	}
+	return 0
+}
+
+func Split(r rune) bool {
+	return r == '.' || r == '"' || r == '?' || r == ',' || r == ' ' || r == '-' || r == '/'
+}
+
+func printFile(fp *os.File, word string) filepath.WalkFunc {
+	return func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			log.Print(err)
+			return nil
+		}
+		if !f.IsDir() {
 			matched, err := regexp.MatchString(".txt", f.Name())
 			if err == nil && matched {
-                tF := new(txtFile)
-                tF.fname = f.Name()
-                tF.numLines,err = lineCounter(path)
+				tF := new(txtFile)
+				tF.fname = f.Name()
+				tF.lineCount, tF.wordCount, err = lineWordCounter(path, word)
 				if err != nil {
 					log.Fatal(err)
 				}
-                writeToFile(fp, tF)
+				writeToFile(fp, tF)
 			}
 		}
 		return nil
-    }
+	}
 }
 
-func  writeToFile(file *os.File, tf *txtFile){
-    fmt.Fprintf(file, "%s,%d,%d\n",tf.fname,tf.numLines)
+func writeToFile(file *os.File, tf *txtFile) {
+	fmt.Fprintf(file, "%s,%d,%d\n", tf.fname, tf.lineCount, tf.wordCount)
 }
 
 func writeHeader(file *os.File) {
-    fmt.Fprintf(file,"File Name, Number of Lines\n")
+	fmt.Fprintf(file, "File Name, Number of Lines, Number of Words\n")
 }
-func safe_open_file() *os.File{
-    file, err := os.Create("result.csv")
-    if err != nil{
-        log.Fatal(err)
-    }
-    return file
+func safe_open_file() *os.File {
+	file, err := os.Create("result.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
 }
-func close_file(file *os.File){
-    err:=file.Close()
-    if err!=nil{
-        log.Fatal(err)
-    }
+func close_file(file *os.File) {
+	err := file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func main(){
-    start := time.Now()
-    
-    var filedir string
-    flag.StringVar(&filedir,"filedir","","Directory you wish to access")
-    //flag.StringVar(&wordPtr,"word","gutenberg","A specific word you wish to find")
-    //flag.Parse()
-    //filedir := flag.Arg[1]
-    //word:=flag.Args(2)
-    flag.Parse()
-    if filedir == ""{
-        log.Fatal("not enough argument")
-    }
+func main() {
+	start := time.Now()
 
-    fp := safe_open_file()
-    writeHeader(fp)
-    err:=filepath.Walk(filedir, printFile(fp))
-    if err!=nil{
-        log.Fatal(err)
-    }
-    close_file(fp)
+	var filedir, keyWord string
+	flag.StringVar(&filedir, "filedir", "", "Directory you wish to access")
+	flag.StringVar(&keyWord, "word", "gutenberg", "A specific word you wish to find")
+	flag.Parse()
+	if filedir == "" || keyWord == "" {
+		log.Fatal("not enough argument")
+	}
 
-    elapsed := time.Since(start)
+	fp := safe_open_file()
+	writeHeader(fp)
+	err := filepath.Walk(filedir, printFile(fp, keyWord))
+	if err != nil {
+		log.Fatal(err)
+	}
+	close_file(fp)
+
+	elapsed := time.Since(start)
 	log.Printf("took %s", elapsed)
 }
-
