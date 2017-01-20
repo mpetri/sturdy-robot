cd3a871b04443616472a2ca12fb4d41caf096859
Matthias Petri <Matthias.Petri@gmail.com>
2017-01-20 21:20:15 +1100

    Fix tests

--- sampleproj_test.go
+++ sampleproj_test.go
@@ -1,28 +1,24 @@
 package main
 
-import "testing" 
+import "testing"
 
-   
+type counterTest struct {
+        Name              string
+        expectedLineCount int
+        expectedWordCount int
+}
+
+var tests = []counterTest{
+        {"test1.txt", 36, 13},
+        {"test2.txt", 43, 23},
+        {"test3.txt", 22, 4},
+}
 
 func TestlineWordCounter(t *testing.T) {
-    counterTests := []counterTest struct{
-    fName string
-    expectedLineCount int
-    expectedWordCount int
-}
-    {
-    counterTest{"test1.txt",36,13,},
-    
-    counterTest{"test2.txt",43,23,},
-    
-    counterTest{"test3.txt",22,4,},
-    }
-
-    for _, cT:= range counterTests{
-        actualLineCount, actualWordCount,_:=lineWordCounter(cT.fName,"GUTENberG")
-        if actualLineCount != cT.expectedLineCount||actualWordCount != cT.expectedWordCount {
-            t.Errorf("lineWordCounter(%s): expectedLineCount %d, expectedWordCount %d, actualLineCount %d
-            ,actualWordCount %d",cT.fName,cT.expectedLineCount,cT.expectedWordCount,actualLineCount,actualWordCount)
+        for _, cT := range tests {
+                actualLineCount, actualWordCount, _ := lineWordCounter(cT.Name, "GUTENberG")
+                if actualLineCount != cT.expectedLineCount || actualWordCount != cT.expectedWordCount {
+                        t.Errorf("lineWordCounter(%s): expectedLineCount %d, expectedWordCount %d, actualLineCount %d ,actualWordCount %d", cT.Name, cT.expectedLineCount, cT.expectedWordCount, actualLineCount, actualWordCount)
+                }
         }
-    }
-} 
+}
